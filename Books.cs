23712299ecf0ee3bbae8577947cd4377b5826ae5
Ralph <sleepwalkinggeek@gmail.com>
2017-02-02 14:53:30 -0600

    Add files via upload
    Books Class

---
+++ Books.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCardCatalog
+{
+    class Books
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Fiction { get; set; }
+        public string NonFiction { get; set; }
+        public int YearPublished { get; set; }
+
+            public Books()
+            {
+
+            }
+
+            public Books(string title, string author, string fiction, string nonfiction, int year)
+            {
+                this.Title = title;
+                this.Author = author;
+                this.Fiction = fiction;
+                this.NonFiction = nonfiction;
+                this.YearPublished = year;
+            }
+        }
+}
